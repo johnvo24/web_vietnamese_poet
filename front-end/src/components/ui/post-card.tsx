62ee990eb36374011ef7933427f433ae595310ae
chjcfcaloc2020 <nguyentruongchi83@gmail.com>
2024-12-25 05:23:52 +0700

    Chi updated

--- front-end/src/components/ui/post-card.tsx
+++ front-end/src/components/ui/post-card.tsx
@@ -6,22 +6,25 @@ import { useRouter } from "next/navigation";
 import { FaRegBookmark, FaRegHeart, FaRegShareSquare } from "react-icons/fa";
 import { PiShareFat } from "react-icons/pi";
 import { RiShareForwardLine } from "react-icons/ri";
+import { api } from "@/lib/utils";
 
 const PostCard = ({ className, poemData }: { className: string, poemData: any }) => {
   const router = useRouter();
 
-  // const poemData = {
-  //   userId: 1,
-  //   genreId: 1,
-  //   userName: "Johnny Dark",
-  //   prompt: "Thơ lục bát về tình yêu",
-  //   title: "Nhặt Tình Lục Bát",
-  //   image: "https://cdn.vectorstock.com/i/1000v/16/84/romantic-background-vector-27211684.jpg",
-  //   content: "Thương ai ngày tháng đợi chờ?\nCho ta say nhớ vần thơ nghĩa tình\nTừ ấy Lục Bát lung linh\nĐể Ta nhặt lấy cho mình chơi vơi!\nLục tình Bát nghĩa ai rơi?\nRồi gieo nhung nhớ cho đời thắm tươi\nTình yêu muôn sắc rạng ngời\nThơ tình Lục bát luôn cười đón em.\nLục bát…rơi chi… lệ mềm!\nCho mi em vướng… ái êm giọt tình\nNgày đêm nỗi nhớ riêng mình\nVần thơ Lục bát thắm in vào hồn.\nDù ai lý lẽ ngoan khôn!!!\nThơ ta Lục bát ru hồn mộng yêu\nSắc màu nắng ngã tím chiều\nVần thơ ta mãi Lục tình Bát thương…",
-  //   note: "",
-  //   createAt: "Dec 20 · 12:19 PM"
-  // }
-  
+  const handleSaveToCol = async () => {
+    try {
+      const token = localStorage.getItem("token")
+      const response = await api.post("/collection/", { poem_id: poemData.poem_id }, {
+        headers: { Authorization: `Bearer ${token}`},
+      })
+      if (response != null) {
+        alert("Poem added to collection successfully")
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
   return (
     <div className={`${className} post-card p-2 bg-white rounded-lg shadow-sm`}>
       <div className="post-header flex justify-between p-2">
@@ -65,7 +68,7 @@ const PostCard = ({ className, poemData }: { className: string, poemData: any })
         </button>
 
         {/* Nút Save */}
-        <button className="action-btn flex flex-1 items-center justify-center space-x-2 hover:bg-gray-200 p-2 rounded-md">
+        <button onClick={handleSaveToCol} className="action-btn flex flex-1 items-center justify-center space-x-2 hover:bg-gray-200 p-2 rounded-md">
           <FaRegBookmark className="text-gray-600" size={20} />
           {/* <FaBookmark className="text-yellow-500" size={20} /> */}
           <span className="w-16 text-start text-gray-700 font-medium">45</span>
